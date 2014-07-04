35498f2f2c6c6cb386fa14601e8387b05a7108c2
Grant Ammons <gammons@gmail.com>
2014-07-04 10:01:32 -0400

    awesome, works

--- src/js/game.js
+++ src/js/game.js
@@ -1,81 +1,86 @@
 /// <reference path="phaser.d.ts"/>
+var Phaser;
+(function (Phaser) {
+    var Game = (function () {
+        function Game() {
+        }
+        return Game;
+    })();
+})(Phaser || (Phaser = {}));
+
 var CrimboGame = (function () {
     function CrimboGame() {
+        var _this = this;
+        this.preload = function () {
+            _this.game.load.image('mushroom', '/assets/mushroom32x32.png');
+            _this.game.load.tilemap('map', 'assets/tilemaps/maps/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
+            _this.game.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
+            _this.game.load.image('phaser', 'assets/sprites/phaser-dude.png');
+        };
+        this.create = function () {
+            // set up tilemap
+            var map = _this.game.add.tilemap('map');
+            map.addTilesetImage('ground_1x1');
+            var layer = map.createLayer('Tile Layer 1');
+            layer.resizeWorld();
+
+            //set up sprite
+            _this.sprite = _this.game.add.sprite(32, 32, 'mushroom');
+            window.sprite = _this.sprite;
+            _this.game.camera.follow(_this.sprite);
+        };
+        this.setMoving = function () {
+            if (_this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+                _this.moving = "right";
+            }
+            if (_this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+                _this.moving = "left";
+            }
+            if (_this.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+                _this.moving = "up";
+            }
+            if (_this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+                _this.moving = "down";
+            }
+        };
+        this.update = function () {
+            if (_this.moving === null) {
+                _this.setMoving();
+            } else {
+                _this.move();
+            }
+        };
+        this.move = function () {
+            switch (_this.moving) {
+                case "right":
+                    _this.sprite.x += CrimboGame.MoveSpeed;
+                    if (_this.sprite.x % 32 === 0)
+                        _this.moving = null;
+                    break;
+                case "left":
+                    _this.sprite.x -= CrimboGame.MoveSpeed;
+                    if (_this.sprite.x % 32 === 0)
+                        _this.moving = null;
+                    break;
+                case "up":
+                    _this.sprite.y -= CrimboGame.MoveSpeed;
+                    if (_this.sprite.y % 32 === 0)
+                        _this.moving = null;
+                    break;
+                case "down":
+                    _this.sprite.y += CrimboGame.MoveSpeed;
+                    if (_this.sprite.y % 32 === 0)
+                        _this.moving = null;
+            }
+        };
+        this.render = function () {
+            _this.game.debug.spriteInfo(_this.sprite, 20, 32);
+        };
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
             preload: this.preload, create: this.create, update: this.update, render: this.render });
+        this.moving = null;
     }
-    CrimboGame.prototype.preload = function () {
-        this.game.load.image('mushroom', '/assets/mushroom32x32.png');
-        this.game.load.tilemap('map', 'assets/tilemaps/maps/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
-        this.game.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
-        this.game.load.image('phaser', 'assets/sprites/phaser-dude.png');
-    };
-
-    CrimboGame.prototype.create = function () {
-        // set up tilemap
-        var map = this.game.add.tilemap('map');
-        map.addTilesetImage('ground_1x1');
-        var layer = map.createLayer('Tile Layer 1');
-        layer.resizeWorld();
-
-        //set up sprite
-        this.sprite = this.game.add.sprite(32, 32, 'mushroom');
-        window.sprite = this.sprite;
-        this.game.camera.follow(this.sprite);
-        // set up physics
-        // this.game.physics.startSystem(Phaser.Physics.P2JS);
-        // this.game.physics.p2.enable(this.sprite);
-        // //this.sprite.body.setZeroDamping();
-        // this.sprite.body.damping = 0.5;
-        // this.sprite.body.fixedRotation = true;
-    };
-
-    CrimboGame.prototype.update = function () {
-        switch (this.moving) {
-            case "right":
-                this.sprite.x += 2;
-                if (this.sprite.x % 32 === 0)
-                    this.moving = null;
-                break;
-        }
-
-        // while (this.sprite.x % 32 !== 0)
-        //   this.sprite.x += 2;
-        //     if ((this.sprite.body.velocity.x > 0) || (this.sprite.body.velocity.y > 0)) { return; }
-        //     //this.sprite.body.setZeroVelocity();
-        //     if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
-        //     {
-        //       this.sprite.body.moveLeft(32);
-        //     }
-        //if (this.moving == null) this.setMoving();
-        if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            this.moving = "right";
-            //this.sprite.body.moveRight(32);
-        }
-    };
-
-    CrimboGame.prototype.setMoving = function () {
-        if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            this.moving = "right";
-            //this.sprite.body.moveRight(32);
-        }
-        //     if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
-        //     {
-        //       this.sprite.body.moveUp(32);
-        //     }
-        //     else if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
-        //     {
-        //       this.sprite.body.moveDown(32);
-        //     }
-        //     else if (this.game.input.keyboard.isDown(69))
-        //     {
-        //
-        //     }
-    };
-    CrimboGame.prototype.render = function () {
-        //this.game.debug.cameraInfo(this.game.camera, 32, 32);
-        this.game.debug.spriteInfo(this.sprite, 20, 32);
-    };
+    CrimboGame.MoveSpeed = 4;
     return CrimboGame;
 })();
 
--- src/ts/game.ts
+++ src/ts/game.ts
@@ -1,92 +1,80 @@
 /// <reference path="phaser.d.ts"/>
 
-class CrimboGame {
-
-  constructor() {
-    this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update:
-      this.update, render: this.render});
+module Phaser {
+  class Game {
+    crimbo: CrimboGame;
   }
+}
+
+class CrimboGame {
+  static MoveSpeed = 4;
 
   game: Phaser.Game;
   sprite: Phaser.Sprite;
   moving: String;
 
+  constructor() {
+    this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update:
+      this.update, render: this.render});
+    this.moving = null;
+  }
 
-
-  preload() {
+  preload = () => {
     this.game.load.image('mushroom', '/assets/mushroom32x32.png');
     this.game.load.tilemap('map', 'assets/tilemaps/maps/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
     this.game.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
     this.game.load.image('phaser', 'assets/sprites/phaser-dude.png');
   }
 
-  create() {
+  create = () => {
     // set up tilemap
     var map = this.game.add.tilemap('map');
     map.addTilesetImage('ground_1x1');
     var layer =  map.createLayer('Tile Layer 1');
     layer.resizeWorld();
 
-
     //set up sprite
     this.sprite =  this.game.add.sprite(32, 32, 'mushroom');
     window.sprite = this.sprite;
     this.game.camera.follow(this.sprite);
-
-    // set up physics
-    // this.game.physics.startSystem(Phaser.Physics.P2JS);
-    // this.game.physics.p2.enable(this.sprite);
-    // //this.sprite.body.setZeroDamping();
-    // this.sprite.body.damping = 0.5;
-    // this.sprite.body.fixedRotation = true;
   }
 
-  update() {
+  setMoving = () => {
+    if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) { this.moving = "right"; }
+    if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) { this.moving = "left"; }
+    if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP)) { this.moving = "up"; }
+    if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) { this.moving = "down"; }
+  }
+
+  update = () => {
+    if (this.moving === null) { 
+      this.setMoving(); 
+    } else {
+      this.move();
+    }
+  }
+
+  move = () => {
     switch(this.moving) {
       case "right":
-        this.sprite.x += 2;
+        this.sprite.x += CrimboGame.MoveSpeed;
         if (this.sprite.x % 32 === 0) this.moving = null;
         break;
-    }
-
-    // while (this.sprite.x % 32 !== 0)
-    //   this.sprite.x += 2;
-//     if ((this.sprite.body.velocity.x > 0) || (this.sprite.body.velocity.y > 0)) { return; }
-//     //this.sprite.body.setZeroVelocity();
-//     if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
-//     {
-//       this.sprite.body.moveLeft(32);
-//     }
-    //if (this.moving == null) this.setMoving();
-    if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
-    {
-      this.moving = "right";
-      //this.sprite.body.moveRight(32);
+      case "left":
+        this.sprite.x -= CrimboGame.MoveSpeed;
+        if (this.sprite.x % 32 === 0) this.moving = null;
+        break;
+      case "up":
+        this.sprite.y -= CrimboGame.MoveSpeed;
+        if (this.sprite.y % 32 === 0) this.moving = null;
+        break;
+      case "down":
+        this.sprite.y += CrimboGame.MoveSpeed;
+        if (this.sprite.y % 32 === 0) this.moving = null;
     }
   }
 
-  setMoving() {
-    if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
-    {
-      this.moving = "right";
-      //this.sprite.body.moveRight(32);
-    }
-
-//     if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
-//     {
-//       this.sprite.body.moveUp(32);
-//     }
-//     else if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
-//     {
-//       this.sprite.body.moveDown(32);
-//     }
-//     else if (this.game.input.keyboard.isDown(69))
-//     {
-//
-//     }
-  }
-  render() {
-    //this.game.debug.cameraInfo(this.game.camera, 32, 32);
+  render = () => {
     this.game.debug.spriteInfo(this.sprite, 20, 32);
   }
 }
